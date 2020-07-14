456512ed30335a15259fa53f9d3759d4f692c84b
Maximiliano-Franco <maxif.franco@gmail.com>
2020-07-13 22:49:25 -0300

    2020-07-13

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,8 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hello world new");
+	alert("The first one");
+	alert("The second one");
+	alert("The third one"); 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,12 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	
+	let nombre; // "let" es reemplazo de "var". Se utiliza para variables
+
+	nombre = prompt ("Ingrese su nombre"); // "prompt" es una ventana desplegable que deja introducir informacion, combinado con "alert" da una respuesta
+
+
+	alert(nombre); // "alert" es una ventana desplegable que solo arroja una informacion
 }
 
