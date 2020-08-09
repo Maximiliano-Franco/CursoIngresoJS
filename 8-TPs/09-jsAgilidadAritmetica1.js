3c5595bf1f266a5bcf58342f01d52938f32b2d26
Maximiliano-Franco <maxif.franco@gmail.com>
2020-08-09 17:46:55 -0300

    Examen

--- 8-TPs/09-jsAgilidadAritmetica1.js
+++ 8-TPs/09-jsAgilidadAritmetica1.js
@@ -6,16 +6,56 @@ En el cuadro de texto resultado el jugador debe ingresar
 el resultado de la operación y presionar el botón Aceptar.
 se debe informar si el resultado es el correcto o no.
 */
-let respuesta;
+let num1;
+let num2;
+let operaciones;
+let resultado;
+let resultadoUser
 
 function comenzar()
 {
+    operaciones = Math.round(Math.random() * 3+1);
+    num1 = Math.round(Math.random() * 9+1);
+    num2 = Math.round(Math.random() * 9+1);
+    
+    switch (operaciones){ 
+	    case 1:
+            resultado = (num1 + num2);
+            operaciones = "+";
+	        break;
+	      
+	    case 2:
+            resultado = (num1 - num2);
+            operaciones = "-";
+	        break;
+	      
+	    case 3:
+            resultado = (num1 * num2);
+            operaciones = "x";
+	        break;
+	      
+	    case 4:
+            resultado = (num1 / num2);
+            operaciones = "/";
+	        break;
+    }
+    
 
-    
-    
+    document.getElementById("txtIdPrimerNumero").value = num1;
+    document.getElementById("txtIdSegundoNumero").value = num2;
+    document.getElementById("txtIdOperador").value = operaciones;
 }
+
 function Responder()
 {
-	
+    
+    resultadoUser = parseInt(document.getElementById("txtIdRespuesta").value);
 
+    if(resultado == resultadoUser){
+        alert("El resultado es correcto");
+    }
+    else {
+        alert("El resultado es incorrecto");
+    }
+    
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,142 @@
 function mostrar()
 {
-  alert("dos");
+  let cant;
+  let cantTotal = 0;
+  let cantArena = 0;
+  let cantCal = 0;
+  let cantCemento = 0;
+  let descuento;
+  let precioIngresado;
+  let precioArena = 0;
+  let precioCal = 0;
+  let precioCemento = 0;
+  let precioBruto;
+  let precioFinal;
+  let producto;
+  let productoMasCant;
+  let productoMasCaro;
+  let seguir;
+
+  do {
+
+    // Le dejo al usuario elejir el producto, la cantidad, y el precio a gusto
+
+
+      producto = prompt("Ingrese producto: Arena, Cal, Cemento").toLowerCase();
+
+              while(producto != "arena" && producto != "cal" && producto != "cemento"){
+                producto = prompt("Error. Ingrese producto: Arena, Cal, Cemento").toLowerCase();
+              }
+
+
+      cant = parseInt(prompt("Ingrese cantidad"));
+        
+              while(cant == isNaN){
+                cant = parseInt(prompt("Eso no es un numero. Ingrese cantidad"));
+              }
+
+            cantTotal += cant;
+
+
+      precioIngresado = parseInt(prompt("Ingrese precio por bolsa"));
+
+              while(precioIngresado == isNaN){
+                precioIngresado = parseInt(prompt("Eso no es un numero. Ingrese precio por bolsa"));
+              }
+
+
+
+    // Sumo cantidad de bolsas y precio segun el producto          
+
+      if (producto == "arena"){
+        cantArena = cant;
+        precioArena = precioIngresado * cantArena;
+      }
+      else if (producto == "cal"){
+        cantCal = cant;
+        precioCal = precioIngresado * cantCal;
+      } 
+      else {
+        cantCemento = cant;
+        precioCemento = precioIngresado * cantCemento;
+      }
+
+
+
+    // Le pregunto al usuario si quiere seguir ingresando productos
+
+          seguir = prompt("Desea continuar introduciendo elementos? Introduzca S/N.")
+
+
+  } while (seguir == "s" || seguir == "S")
+
+
+  // Establezco el precio sin descuento
+
+          precioBruto = precioArena + precioCemento + precioCal;
+
+
+
+  // Establezo el precio con el descuento aplicado
+
+          if(cantTotal > 0 && cantTotal < 10 ){
+            descuento = 1;
+            precioFinal = precioBruto * descuento;
+          }
+
+          else if (cantTotal >= 10 && cantTotal < 30){
+            descuento = 0.15;
+            precioFinal = precioBruto * descuento;
+          }
+
+          else if(cantTotal > 30){
+            descuento = 0.25;
+            precioFinal = precioBruto * descuento;
+          }
+
+          else {
+            precioFinal = 0
+          }
+
+
+
+  // Establezco el producto con mas cantidad de bolsas
+
+          if(cantArena > cantCal && cantArena > cantCemento){
+            productoMasCant = "Arena";
+          } 
+
+          else if (cantCal > cantArena && cantCal > cantCemento){
+            productoMasCant = "Cal";
+          }
+
+          else {
+            productoMasCant = "Cemento";
+          }
+
+
+
+  // Establezo el Producto mas caro    
+
+          if(precioArena > precioCal && precioArena > precioCemento){
+            productoMasCaro = "Arena";
+          } 
+
+          else if (precioCal > precioArena && precioCal > precioCemento){
+            productoMasCaro = "Cal";
+          }
+
+          else {
+            productoMasCaro = "Cemento";
+          }
+
+    
+  // Brindo datos al usuario
+
+      alert("Cantidad de bolsas de arena: " + cantArena + "\n" + "Cantidad de bolsas de cal: " + cantCal + "\n" +
+            "Cantidad de bolsas de cemento: " + cantCemento + "\n" + "Cantidad total de bolsas: " + cantTotal + "\n" +
+            "El producto con mas cantidad de bolsas es: " + productoMasCant + "\n" +
+            "El producto mas caro es: " + productoMasCaro + "\n" + "El precio bruto es: $" + precioBruto + "\n" +
+            "El precio con descuento es: $" + precioFinal + "Porque se le esta aplicando " + descuento + "% de descuento");
+
 }
